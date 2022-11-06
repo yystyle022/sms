8a9b64ac96456235c0eaf45fb2fa7f00fd442908
ext.yangyang16 <ext.yangyang16@jd.com>
2022-11-07 00:12:33 +0800

    hhhhh

--- app/src/androidTest/java/com/jdd/sms/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jdd/sms/ExampleInstrumentedTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 //这个是测试文件hahahqwertyuiop
-123333333333
+12333333333325566
 /**
  * Instrumented test, which will execute on an Android device.
  *
